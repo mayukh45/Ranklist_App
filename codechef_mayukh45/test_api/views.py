c2dd4393eb1186f23aba0d5ab4de097d85d9fa67
mayukh45 <mayukh1110@gmail.com>
2018-09-15 23:52:11 +0800

    Sync

--- codechef_mayukh45/test_api/views.py
+++ codechef_mayukh45/test_api/views.py
@@ -1,14 +1,11 @@
-from django.http import HttpResponse
+
 from django.shortcuts import  render
 from codechef_mayukh45.conf import access_token,refresh_token
-from codechef_mayukh45.MAIN import get_contests
+
 
 
 def index(request):
-    contests = get_contests(access_token,refresh_token)
-
-    context = {'contests':contests}
-    return render(request,"home_page.html", context)
+   return render(request,"home_page.html")
 
 
 
